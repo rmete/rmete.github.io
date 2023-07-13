39d3e413c9d088c2f78a2618fbb402923ce9421d
Riley Mete <riley.mete@c3.ai>
2023-07-12 21:22:33 -0700

    Adding files

--- d3code.js
+++ d3code.js
@@ -1,77 +1,3 @@
-
-
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-
-// var parseDate = d3.timeParse("%Y-%m-%d");
-
-// var x = d3.scaleTime().range([0, width]);
-
-// var y = d3.scaleLinear().range([height, 0]);
-
-// var xAxis = d3.axisBottom(x);
-
-// var yAxis = d3.axisLeft(y);
-
-// var line = d3.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.value); });
-
-// var svg = d3.select("#chart").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// function drawChart(dataFile) {
-//   svg.selectAll('*').remove();
-
-//   d3.csv(dataFile).then(function(data) {
-//     data.forEach(function(d) {
-//       d.date = parseDate(d.date);
-//       d.value = +d.value;
-//     });
-
-//     x.domain(d3.extent(data, function(d) { return d.date; }));
-//     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(xAxis);
-
-//     svg.append("g")
-//         .attr("class", "y axis")
-//         .call(yAxis)
-//       .append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", 6)
-//         .attr("dy", ".71em")
-//         .style("text-anchor", "end")
-//         .text("Close");
-
-//     svg.append("path")
-//         .datum(data)
-//         .attr("class", "line")
-//         .attr("d", line);
-//   }).catch(function(error){
-//     console.log(error);
-//   });
-// }
-
-// document.getElementById('load-data-1987').addEventListener('click', function() {
-//   drawChart("SP500_1987.csv");
-// });
-
-// document.getElementById('load-data-1929').addEventListener('click', function() {
-//   drawChart("DOW_1929.csv");
-// });
-
-// // Load the 1929 data by default
-// drawChart("DOW_1929.csv");
-
-
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -95,7 +21,6 @@ var svg = d3.select("#chart").append("svg")
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
 function drawChart(dataFile) {
   svg.selectAll('*').remove();
@@ -128,33 +53,13 @@ function drawChart(dataFile) {
         .datum(data)
         .attr("class", "line")
         .attr("d", line);
-
-    var makeAnnotations = d3.annotation()
-      .type(d3.annotationLabel)
-      .annotations(data.map(function(d) { 
-        return {
-          note: {
-            label: `${d.value}`,
-            title: `Date: ${d.date.toISOString().slice(0,10)}`,
-          },
-          x: x(d.date),
-          y: y(d.value),
-          dy: 30,
-          dx: 30
-        }
-      }));
-
-    svg.append("g")
-      .attr("class", "annotation-group")
-      .call(makeAnnotations);
-
   }).catch(function(error){
     console.log(error);
   });
 }
 
 document.getElementById('load-data-1987').addEventListener('click', function() {
-  drawChart("SP500_1987_2.csv");
+  drawChart("SP500_1987.csv");
 });
 
 document.getElementById('load-data-1929').addEventListener('click', function() {
