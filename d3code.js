af662bdc7fb25fec53eb4922ec6c1525a512aaa2
Riley Mete <riley.mete@c3.ai>
2023-07-13 07:09:00 -0700

    Added extra buttons

--- d3code.js
+++ d3code.js
@@ -1,480 +1,3 @@
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-
-// var parseDate = d3.timeParse("%Y-%m-%d");
-
-// var x = d3.scaleTime().range([0, width]);
-
-// var y = d3.scaleLinear().range([height, 0]);
-
-// var xAxis = d3.axisBottom(x);
-
-// var yAxis = d3.axisLeft(y);
-
-// var line = d3.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.value); });
-
-// var svg = d3.select("#chart").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// function drawChart(dataFile) {
-//   svg.selectAll('*').remove();
-
-//   d3.csv(dataFile).then(function(data) {
-//     data.forEach(function(d) {
-//       d.date = parseDate(d.date);
-//       d.value = +d.value;
-//     });
-
-//     x.domain(d3.extent(data, function(d) { return d.date; }));
-//     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(xAxis);
-
-//     svg.append("g")
-//         .attr("class", "y axis")
-//         .call(yAxis)
-//       .append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", 6)
-//         .attr("dy", ".71em")
-//         .style("text-anchor", "end")
-//         .text("Close");
-
-//     svg.append("path")
-//         .datum(data)
-//         .attr("class", "line")
-//         .attr("d", line);
-//   }).catch(function(error){
-//     console.log(error);
-//   });
-// }
-
-// document.getElementById('load-data-1987').addEventListener('click', function() {
-//   drawChart("SP500_1987.csv");
-// });
-
-// document.getElementById('load-data-1929').addEventListener('click', function() {
-//   drawChart("DOW_1929.csv");
-// });
-
-// // Load the 1929 data by default
-// drawChart("DOW_1929.csv");
-
-// // // //////////////////////////////////////////////////////////////////////////////////
-// // // var margin = {top: 20, right: 20, bottom: 30, left: 50},
-// // //     width = 960 - margin.left - margin.right,
-// // //     height = 500 - margin.top - margin.bottom;
-
-// // // var parseDate = d3.timeParse("%Y-%m-%d");
-
-// // // var x = d3.scaleTime().range([0, width]);
-
-// // // var y = d3.scaleLinear().range([height, 0]);
-
-// // // var xAxis = d3.axisBottom(x);
-
-// // // var yAxis = d3.axisLeft(y);
-
-// // // var line = d3.line()
-// // //     .x(function(d) { return x(d.date); })
-// // //     .y(function(d) { return y(d.value); });
-
-// // // var svg = d3.select("#chart").append("svg")
-// // //     .attr("width", width + margin.left + margin.right)
-// // //     .attr("height", height + margin.top + margin.bottom)
-// // //   .append("g")
-// // //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// // // var tooltip = d3.select("body").append("div")
-// // //     .attr("class", "tooltip")
-// // //     .style("opacity", 0);
-
-// // // function drawChart(dataFile) {
-// // //   svg.selectAll('*').remove();
-
-// // //   d3.csv(dataFile).then(function(data) {
-// // //     data.forEach(function(d) {
-// // //       d.date = parseDate(d.date);
-// // //       d.value = +d.value;
-// // //     });
-
-// // //     x.domain(d3.extent(data, function(d) { return d.date; }));
-// // //     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-// // //     svg.append("g")
-// // //         .attr("class", "x axis")
-// // //         .attr("transform", "translate(0," + height + ")")
-// // //         .call(xAxis);
-
-// // //     svg.append("g")
-// // //         .attr("class", "y axis")
-// // //         .call(yAxis)
-// // //       .append("text")
-// // //         .attr("transform", "rotate(-90)")
-// // //         .attr("y", 6)
-// // //         .attr("dy", ".71em")
-// // //         .style("text-anchor", "end")
-// // //         .text("Close");
-
-// // //     svg.append("path")
-// // //         .datum(data)
-// // //         .attr("class", "line")
-// // //         .attr("d", line);
-
-// // //     var overlay = svg.append("rect")
-// // //       .attr("class", "overlay")
-// // //       .attr("width", width)
-// // //       .attr("height", height)
-// // //       .style("fill", "none")
-// // //       .style("pointer-events", "all");
-
-// // //     var focus = svg.append("g")
-// // //       .style("display", "none");
-
-// // //     focus.append("circle")
-// // //       .attr("r", 4.5);
-
-// // //     overlay
-// // //       .on("mouseover", function() { focus.style("display", null); tooltip.style("opacity", .9); })
-// // //       .on("mouseout", function() { focus.style("display", "none"); tooltip.style("opacity", 0); })
-// // //       .on("mousemove", function() {
-// // //           var mouse = d3.mouse(this);
-// // //           var x0 = x.invert(mouse[0]);
-// // //           var y0 = y.invert(mouse[1]);
-// // //           focus.attr("transform", "translate(" + x(mouse[0]) + "," + y(mouse[1]) + ")");
-
-// // //           tooltip.html("x: " + x0 + "<br/>y: " + y0)
-// // //               .style("left", (d3.event.pageX + 5) + "px")
-// // //               .style("top", (d3.event.pageY - 28) + "px");
-// // //       });
-
-// // //   }).catch(function(error){
-// // //     console.log(error);
-// // //   });
-// // // }
-
-// // // document.getElementById('load-data-1987').addEventListener('click', function() {
-// // //   drawChart("SP500_1987_2.csv");
-// // // });
-
-// // // document.getElementById('load-data-1929').addEventListener('click', function() {
-// // //   drawChart("DOW_1929.csv");
-// // // });
-
-// // // // Load the 1929 data by default
-// // // drawChart("DOW_1929.csv");
-// // var margin = {top: 20, right: 20, bottom: 30, left: 50},
-// //     width = 960 - margin.left - margin.right,
-// //     height = 500 - margin.top - margin.bottom;
-
-// // var parseDate = d3.timeParse("%Y-%m-%d");
-
-// // var x = d3.scaleTime().range([0, width]);
-// // var y = d3.scaleLinear().range([height, 0]);
-
-// // var xAxis = d3.axisBottom(x);
-// // var yAxis = d3.axisLeft(y);
-
-// // var line = d3.line()
-// //     .x(function(d) { return x(d.date); })
-// //     .y(function(d) { return y(d.value); });
-
-// // var svg = d3.select("#chart").append("svg")
-// //     .attr("width", width + margin.left + margin.right)
-// //     .attr("height", height + margin.top + margin.bottom)
-// //   .append("g")
-// //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// // // Tool tip
-// // var focus = svg.append('g').style('display', 'none');
-
-// // focus.append('circle')
-// //     .attr('r', 4.5);
-
-// // focus.append('text')
-// //     .attr('x', 9)
-// //     .attr('dy', '.35em');
-
-// // // Overlay for 'mousemove' event
-// // var focusRect = svg.append('rect')
-// //     .attr('width', width)
-// //     .attr('height', height)
-// //     .style('fill', 'none')
-// //     .style('pointer-events', 'all');
-
-// // function drawChart(dataFile) {
-// //   svg.selectAll('*').remove();
-
-// //   d3.csv(dataFile).then(function(data) {
-// //     data.forEach(function(d) {
-// //       d.date = parseDate(d.date);
-// //       d.value = +d.value;
-// //     });
-
-// //     x.domain(d3.extent(data, function(d) { return d.date; }));
-// //     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-// //     svg.append("g")
-// //         .attr("class", "x axis")
-// //         .attr("transform", "translate(0," + height + ")")
-// //         .call(xAxis);
-
-// //     svg.append("g")
-// //         .attr("class", "y axis")
-// //         .call(yAxis)
-// //       .append("text")
-// //         .attr("transform", "rotate(-90)")
-// //         .attr("y", 6)
-// //         .attr("dy", ".71em")
-// //         .style("text-anchor", "end")
-// //         .text("Close");
-
-// //     svg.append("path")
-// //         .datum(data)
-// //         .attr("class", "line")
-// //         .attr("d", line);
-
-// //     var bisectDate = d3.bisector(function(d) { return d.date; }).left;
-
-// //     focusRect.on('mouseover', function() { focus.style('display', null); });
-// //     focusRect.on('mouseout', function() { focus.style('display', 'none'); });
-// //     focusRect.on('mousemove', function(event) {
-// //       var coordinates = d3.pointer(event, this);
-// //       var x0 = x.invert(coordinates[0]);
-// //       var i = bisectDate(data, x0, 1);
-// //       var d0 = data[i - 1];
-// //       var d1 = data[i];
-// //       var d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-// //       focus.attr('transform', `translate(${x(d.date)}, ${y(d.value)})`);
-// //       focus.select('text').text(`${d.value}`);
-// //     });
-
-// //   }).catch(function(error){
-// //     console.log(error);
-// //   });
-// // }
-
-// // document.getElementById('load-data-1987').addEventListener('click', function() {
-// //   drawChart("SP500_1987.csv");
-// // });
-
-// // document.getElementById('load-data-1929').addEventListener('click', function() {
-// //   drawChart("DOW_1929.csv");
-// // });
-
-// // // Load the 1929 data by default
-// // drawChart("DOW_1929.csv");
-
-
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-
-// var parseDate = d3.timeParse("%Y-%m-%d");
-
-// var x = d3.scaleTime().range([0, width]);
-// var y = d3.scaleLinear().range([height, 0]);
-
-// var xAxis = d3.axisBottom(x);
-// var yAxis = d3.axisLeft(y);
-
-// var line = d3.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.value); });
-
-// var svg = d3.select("#chart").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// var focus = svg.append('g').style('display', 'none');
-
-// focus.append('circle')
-//     .attr('r', 4.5);
-
-// var focusText = focus.append('text')
-//     .attr('x', 9)
-//     .attr('dy', '.35em');
-
-// svg.append('rect')
-//     .attr('class', 'overlay')
-//     .attr('width', width)
-//     .attr('height', height)
-//     .style('fill', 'none')
-//     .style('pointer-events', 'all');
-
-// function drawChart(dataFile) {
-
-//   d3.csv(dataFile).then(function(data) {
-//     data.forEach(function(d) {
-//       d.date = parseDate(d.date);
-//       d.value = +d.value;
-//     });
-
-//     x.domain(d3.extent(data, function(d) { return d.date; }));
-//     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(xAxis);
-
-//     svg.append("g")
-//         .attr("class", "y axis")
-//         .call(yAxis)
-//       .append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", 6)
-//         .attr("dy", ".71em")
-//         .style("text-anchor", "end")
-//         .text("Close");
-
-//     svg.append("path")
-//         .datum(data)
-//         .attr("class", "line")
-//         .attr("d", line);
-
-//     var bisectDate = d3.bisector(function(d) { return d.date; }).left;
-
-//     svg.select('.overlay').on('mouseover', function() { focus.style('display', null); });
-//     svg.select('.overlay').on('mouseout', function() { focus.style('display', 'none'); });
-//     svg.select('.overlay').on('mousemove', function(event) {
-//       var x0 = x.invert(d3.pointer(event)[0]);
-//       var i = bisectDate(data, x0, 1);
-//       var d0 = data[i - 1];
-//       var d1 = data[i];
-//       var d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-//       focus.attr('transform', `translate(${x(d.date)}, ${y(d.value)})`);
-//       focusText.text(`${d.value}`);
-//     });
-
-//   }).catch(function(error){
-//     console.log(error);
-//   });
-// }
-
-// document.getElementById('load-data-1987').addEventListener('click', function() {
-//   drawChart("SP500_1987.csv");
-// });
-
-// document.getElementById('load-data-1929').addEventListener('click', function() {
-//   drawChart("DOW_1929.csv");
-// });
-
-// // Load the 1929 data by default
-// drawChart("DOW_1929.csv");
-
-
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
-
-// var parseDate = d3.timeParse("%Y-%m-%d");
-
-// var x = d3.scaleTime().range([0, width]);
-
-// var y = d3.scaleLinear().range([height, 0]);
-
-// var xAxis = d3.axisBottom(x);
-
-// var yAxis = d3.axisLeft(y);
-
-// var line = d3.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.value); });
-
-// var svg = d3.select("#chart").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// var tooltip = d3.select("body").append("div")
-//     .attr("class", "tooltip")
-//     .style("opacity", 0);
-
-// function drawChart(dataFile) {
-//   svg.selectAll('*').remove();
-
-//   d3.csv(dataFile).then(function(data) {
-//     data.forEach(function(d) {
-//       d.date = parseDate(d.date);
-//       d.value = +d.value;
-//     });
-
-//     x.domain(d3.extent(data, function(d) { return d.date; }));
-//     y.domain(d3.extent(data, function(d) { return d.value; }));
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(xAxis);
-
-//     svg.append("g")
-//         .attr("class", "y axis")
-//         .call(yAxis)
-//       .append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", 6)
-//         .attr("dy", ".71em")
-//         .style("text-anchor", "end")
-//         .text("Close");
-
-//     svg.append("path")
-//         .datum(data)
-//         .attr("class", "line")
-//         .attr("d", line);
-
-//     // Invisible circles for tooltips
-//     svg.selectAll("dot")
-//         .data(data)
-//         .enter().append("circle")
-//         .attr("r", 5)
-//         .attr("cx", function(d) { return x(d.date); })
-//         .attr("cy", function(d) { return y(d.value); })
-//         .style("fill", "none")
-//         .style("pointer-events", "all")
-//         .on("mouseover", function(d) {
-//             tooltip.transition()
-//                 .duration(200)
-//                 .style("opacity", .9);
-//             tooltip.html("Date: " + "<br/>Value: " + d.value) //  d.date.toDateString() + 
-//                 .style("left", (d3.event.pageX + 5) + "px")
-//                 .style("top", (d3.event.pageY - 28) + "px");
-//         })
-//         .on("mouseout", function(d) {
-//             tooltip.transition()
-//                 .duration(500)
-//                 .style("opacity", 0);
-//         });
-
-//   }).catch(function(error){
-//     console.log(error);
-//   });
-// }
-
-// document.getElementById('load-data-1987').addEventListener('click', function() {
-//   drawChart("SP500_1987_2.csv");
-// });
-
-// document.getElementById('load-data-1929').addEventListener('click', function() {
-//   drawChart("DOW_1929.csv");
-// });
-
-// // Load the 1929 data by default
-// drawChart("DOW_1929.csv");
-
-
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -507,6 +30,23 @@ var tooltip = d3.select("body").append("div")
 
 function drawChart(dataFile) {
   svg.selectAll('*').remove();
+
+  var title, summary;
+  if(dataFile === "SP500_1987_2.csv") {
+    title = "S&P 500 - 1987";
+    summary = "This chart represents the S&P 500 index for the year 1987...";
+  } else if(dataFile === "DOW_1929.csv") {
+    title = "DOW - 1929";
+    summary = "This chart represents the DOW index for the year 1929...";
+  }
+
+  svg.append("text") 
+    .attr("x", (width / 2))             
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "20px") 
+    .style("font-weight", "bold")  
+    .text(title);
 
   // Create a tooltip dot but set its opacity to 0
   var tooltipDot = svg.append("circle")
@@ -568,6 +108,11 @@ function drawChart(dataFile) {
                    .duration(500)
                    .style("opacity", 0);
         });
+    svg.append("text")
+    .attr("x", 0)             
+    .attr("y", height + margin.bottom)
+    .style("font-size", "16px")   
+    .text(summary);
 
   }).catch(function(error){
     console.log(error);
@@ -582,5 +127,16 @@ document.getElementById('load-data-1929').addEventListener('click', function() {
   drawChart("DOW_1929.csv");
 });
 
+document.getElementById('load-data-2008').addEventListener('click', function() {
+  drawChart("SP500_2008_2.csv");
+});
+
+document.getElementById('load-data-2020').addEventListener('click', function() {
+  drawChart("SP500_2020_2.csv");
+});
+
 // Load the 1929 data by default
 drawChart("DOW_1929.csv");
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
