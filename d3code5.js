4d3dcc8e6ba838ae9c550b20c8d6e552b4dc0494
Riley Mete <riley.mete@c3.ai>
2023-07-19 09:22:22 -0700

    Updated

--- d3code5.js
+++ d3code5.js
@@ -68,12 +68,6 @@ function drawChart(dataFile) {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Close");
 
     svg.append("path")
         .datum(data)
